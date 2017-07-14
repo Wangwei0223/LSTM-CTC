1b3fa94785e24f3791504263a55e20bed137ca24
WWWaeing <291978313@qq.com>
2017-07-14 14:13:13 +0800

    Update train_CRNN.py

--- train_CRNN.py
+++ train_CRNN.py
@@ -16,7 +16,7 @@ num_classes = len(LETTERS) + 1  # 26 digits + ctc blank
 batch_size = 32
 learning_rate = 0.001
 MOMENTUM = 0.9
-n_hidden = 256  # RNN隐藏层神经元 与模型中的对应
+n_hidden = 256  # The hidden units in RNN
 
 # parameter
 
@@ -29,9 +29,9 @@ test_path_label = label_list[0:16]
 
 num_batches_per_epoch = int(num_examples/batch_size)
 
-img = tf.Variable(tf.random_normal([batch_size, 32, 100, 1]))  # 32*100通道1
+img = tf.Variable(tf.random_normal([batch_size, 32, 100, 1]))  # 32*100
 
-x = tf.placeholder(tf.float32, [None, 32, 100, 1])  # 到时的输入input
+x = tf.placeholder(tf.float32, [None, 32, 100, 1])  # as the input
 
 y = model.CNN(x)
 
@@ -39,28 +39,25 @@ W = tf.Variable(tf.truncated_normal(shape=[n_hidden, num_classes], stddev=0.1))
 
 b = tf.Variable(tf.constant(0.1, shape=[num_classes]))
 
-# Here we use sparse_placeholder that will generate a
-# SparseTensor required by ctc_loss op.
-targets = tf.sparse_placeholder(tf.int32)  # 到时的输入label，需要用稀疏矩阵
+# SparseTensor required by CTC layer.
+targets = tf.sparse_placeholder(tf.int32)  # as the label
 
-seq_len = tf.placeholder(tf.int32, [None])  # 1d array of size [batch_size] 说白了32个512[512, 512,..., 512, 512]
+seq_len = tf.placeholder(tf.int32, [None])  # 1d array of size [batch_size] 
 
 inputs = tf.placeholder(tf.float32, [None, None, 512])
 
 cell = tf.contrib.rnn.LSTMCell(n_hidden, state_is_tuple=True)
 
 
-# Stacking rnn cells
 stack = tf.contrib.rnn.MultiRNNCell([cell] * 2,
                                     state_is_tuple=True)
 
-# The second output is the last state and we will no use that
 outputs, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
 
 shape = tf.shape(inputs)
+
 batch_s, max_timesteps = shape[0], shape[1]
 
-# Reshaping to apply the same weights over the timesteps
 outputs = tf.reshape(outputs, [-1, n_hidden])
 
 logits = tf.matmul(outputs, W) + b
@@ -84,9 +81,6 @@ cost = tf.reduce_mean(loss)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(loss)
 
-# Option 2: tf.contrib.ctc.ctc_beam_search_decoder
-# (it's slower but you'll get better results)
-# decoded, log_prob = tf.nn.ctc_greedy_decoder(logits, seq_len)
 decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, seq_len, merge_repeated=False)
 # Inaccuracy: label error rate
 ler = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32),
@@ -104,7 +98,7 @@ def train_network():
             train_cost = train_ler = 0
             for batch in range(num_batches_per_epoch):
                 indexes = [i % num_examples for i in range(batch * batch_size, (batch + 1) * batch_size)]
-                sparse_targets = model.sparse_tuple_from_label(train_targets[indexes])   # 处理完的标签数据 稀疏矩阵
+                sparse_targets = model.sparse_tuple_from_label(train_targets[indexes])   # get the sparse_tragets
                 process_list = np.asarray(train_path)[indexes]
                 resized = pr.preprocess_img(process_list, 32, 100)
 
@@ -136,7 +130,7 @@ def train_network():
 def test_network():
     with tf.Session() as sess:
         saver = tf.train.Saver()
-        path = os.path.abspath('..') + "/Model_CRNN 2/CRNN_MNIST_Image.ckpt-4"
+        path = os.path.abspath('..') + "/Model_CRNN 2/CRNN_MNIST_Image.ckpt-4" # Change your save path
         saver.restore(sess, path)
         batch_test_target = model.sparse_tuple_from_label(test_targets)
         process_test_list = np.asarray(test_path)
